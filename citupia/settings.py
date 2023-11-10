d1f252cc8372563a132c61c9a17c40b86f2f36b5
Abbas Mandasorwala <aam119@student.bham.ac.uk>
2023-11-10 20:22:55 +0000

    added mysql database

--- citupia/settings.py
+++ citupia/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'citupia.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydb',
+        'USER': 'root',
+        'PASSWORD': 'abbas20034',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
 import SignUpView from '@/views/SignUpView.vue';
+import LogInView from "@/views/LogInView.vue";
 
 const routes = [
   { path: '/', name: 'Home', component: HomeView },
   { path: '/signup', name: 'SignUp', component: SignUpView.vue},
+  { path: '/login', name: 'LogIn', component: LogInView.vue}
   // ...add other routes here
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes,
 });
 
