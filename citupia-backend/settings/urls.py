d3b29dbe8bf6963b4d5e6a8e53e7e5f466a2e9c7
Abbas Mandasorwala <aam119@student.bham.ac.uk>
2023-12-16 19:49:15 +0000

    Remove all files from the repository

--- citupia-backend/settings/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'settings'
-urlpatterns = [
-    path('delete_account/', views.delete_account, name='delete_account'),
-    path('change_username/', views.change_username, name='change_username')
-]
