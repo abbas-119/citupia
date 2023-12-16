ebb1046f71300d74215c0dbb5145f3e68d43b8c3
Abbas Mandasorwala <aam119@student.bham.ac.uk>
2023-12-16 16:56:12 +0000

    Remove all files from the repository

--- frontend/src/views/axiosClient.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const axiosClient = axios.create({
-  baseURL: 'http://localhost:8000', // Your Django backend URL
-  // You can add your headers here
-});
-
-export default axiosClient;
