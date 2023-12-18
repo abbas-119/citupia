2d360e1fa04173df5e89e4b23781d6f289fcd7f1
Abbas Mandasorwala <aam119@student.bham.ac.uk>
2023-12-18 13:35:21 +0000

    Vue and tailwind css installed

---
+++ vue-frontend/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <div class="bg-red-500">hi</div>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
